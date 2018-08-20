476aec3a0cbd5f0d2067f58e735a92b5bf795fff
郑进 <1021871291@qq.com>
2018-08-20 20:14:13 +0800

    预览待完成

---
+++ src/containers/Preview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Preview extends Component {
+  render () {
+    return (
+      <div>
+        Preview
+      </div>
+    )
+  }
+}
+
+export default Preview
